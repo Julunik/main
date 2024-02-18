5ab3676018d370b5b8949730343c151d3f0da5f2
Julunik <139068502+Julunik@users.noreply.github.com>
2024-02-18 12:54:43 +0100

    Add files via upload

---
+++ krpg3/main.js
@@ -0,0 +1,3 @@
+const GameLogo = document.getElementById("ikona");
+const icons = ["krpg3_SCALED.png", "krpg3_classic.png", "krpg3_dsc.png"];
+function Kliknieto(){ GameLogo.src = "grafika_krpg3/"+icons[Math.floor(Math.random() * 3)]; }
