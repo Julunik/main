5145f36d2fdad460e4b8fd459c620ee14c590df9
Julunik <139068502+Julunik@users.noreply.github.com>
2024-08-18 11:07:11 +0200

    Delete krpg3/main.js

--- krpg3/main.js
+++
@@ -1,4 +0,0 @@
-const GameLogo = document.getElementById("ikona");
-const icons = ["krpg3_SCALED.png", "krpg3_classic.png", "krpg3_dsc.png"];
-function Kliknieto(){ GameLogo.src = "grafika_krpg3/"+icons[Math.floor(Math.random() * 3)]; }
-console.log("Wersja strony: 7");
