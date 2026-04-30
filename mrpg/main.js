e26cb5851418effc3fc53a32bffaf90f2e5bb370
Julunik <139068502+Julunik@users.noreply.github.com>
2026-04-30 23:03:17 +0200

    Add files via upload

---
+++ mrpg/main.js
@@ -0,0 +1,5 @@
+// const GameLogo = document.getElementById("ikona");
+// const icons = ["krpg3_SCALED.png", "krpg3_classic.png", "krpg3_dsc.png"];
+// GameLogo.src = "grafika_mrpg/"+icons[Math.floor(Math.random() * 3)];
+function Kliknieto(){ console.log("KLIKNIĘTO!"); }
+console.log("Wersja strony: 9");
